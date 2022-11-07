1034ab1f084457634625d9e7569b83c491765c2b
Zach van Noppen <dzachvannoppen@gmail.com>
2022-11-06 20:52:13 -0500

    food spawning + physics

--- MollysPlaygroung/Assets/Scripts/Gameplay/GluttonyLevel.cs
+++ MollysPlaygroung/Assets/Scripts/Gameplay/GluttonyLevel.cs
@@ -6,10 +6,12 @@ using Photon.Pun;
 
 public class GluttonyLevel : MonoBehaviour
 {
-    public GameObject foodItem;
+    public GameObject[] foodItems;
     public float spawnTime = 5f;
     public float spawnDelay = 5f;
     public bool stopSpawning = false;
+    public float spawnHeight = 50f;
+    public float[] mapSize = { 100f, 100f }; //This will determine item spawn range. If the size is 100 x 100, items will spawn between 50 and -50
 
     void Start()
     {
@@ -23,7 +25,14 @@ public class GluttonyLevel : MonoBehaviour
 
     public void SpawnFood()
     {
-        Instantiate(foodItem, transform.position, transform.rotation);
+
+        if(foodItems.Length > 0)
+        {
+            int foodItemNum = Random.Range(0, foodItems.Length);
+            Vector3 spawnPosition = new Vector3(Random.Range(-mapSize[0]/2, mapSize[0]/2), spawnHeight, Random.Range(-mapSize[1] / 2, mapSize[1] / 2));
+            Instantiate(foodItems[foodItemNum], spawnPosition, transform.rotation);
+        }
+        
         if (stopSpawning)
         {
             CancelInvoke("SpawnObject");
