b51124114da231f0f68323c6be9e8cca1a86ab67
natalia199 <ocamponati@Hotmail.com>
2023-01-13 03:41:53 -0500

    Base code for Greed

---
+++ MollysPlaygroung/Assets/Scripts/Gameplay/Greed/GreedGameManager.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreedGameManager : MonoBehaviour
+{
+    public Vector3 diceCameraPos;
+    public Quaternion diceCameraRot;
+    public float diceCameraSize;
+
+    public Vector3 playCameraPos;
+    public Quaternion playCameraRot;
+    public float playCameraSize;
+
+    public GameObject ChipPrefab;
+    public GameObject ChipParent;
+    public List<Transform> ChipSpawnPoints = new List<Transform>();
+
+    public bool diceProcedure;
+
+    public GameObject Dice;
+
+    public int diceValueRolled;
+
+    void Start()
+    {
+        diceProcedure = true;
+        SetPlayersCamera();
+        UpdateChipAmount(10);
+    }
+
+    void Update()
+    {
+        SetPlayersCamera();
+    }
+
+    public void SetPlayersCamera()
+    {
+        if (diceProcedure)
+        {
+
+            GameObject.Find("Main Camera").transform.position = diceCameraPos;
+            GameObject.Find("Main Camera").transform.rotation = diceCameraRot;
+            GameObject.Find("Main Camera").GetComponent<Camera>().orthographicSize = diceCameraSize;
+        }
+        else
+        {
+            GameObject.Find("Main Camera").transform.position = playCameraPos;
+            GameObject.Find("Main Camera").transform.rotation = playCameraRot;
+            GameObject.Find("Main Camera").GetComponent<Camera>().orthographicSize = playCameraSize;
+        }
+    }
+
+    public void RollingTheDice()
+    {
+        Dice.GetComponent<Dice>().RollDice();
+    }
+
+    public void DiceValueUpdate(int value)
+    {
+        diceValueRolled = value;
+    }
+
+    public void FirstCarriedChip(GameObject chip, GameObject player, int chipCount)
+    {
+        Destroy(chip.GetComponent<Rigidbody>());
+
+        chip.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1f, player.transform.position.z);   
+        chip.transform.rotation = Quaternion.identity;
+
+        chip.transform.parent = player.transform;
+        chip.GetComponent<ChipScript>().Available = false;
+    }
+
+    public void CarryMoreChips(GameObject chip, GameObject player, GameObject previousChip, int chipCount)
+    {
+        Destroy(chip.GetComponent<Rigidbody>());
+
+        chip.transform.position = new Vector3(player.transform.position.x, previousChip.transform.position.y + 1f, player.transform.position.z);
+        chip.transform.rotation = Quaternion.identity;
+
+        chip.transform.parent = player.transform;
+        chip.GetComponent<ChipScript>().Available = false;
+    }
+
+    public void DropTheChip(GameObject chip, GameObject player, GameObject zone)
+    {
+        chip.transform.parent = null;
+        chip.AddComponent<Rigidbody>();
+        chip.GetComponent<ChipScript>().Available = true;
+        chip.GetComponent<ChipScript>().throwChip(zone.transform.GetChild(0).position, transform.position);
+    }
+
+    public void ChipInstantiation()
+    {
+        float xPos = Random.Range(ChipSpawnPoints[0].position.x, ChipSpawnPoints[2].position.x);
+        float zPos = Random.Range(ChipSpawnPoints[0].position.z, ChipSpawnPoints[1].position.z);
+
+        GameObject temp = Instantiate(ChipPrefab, new Vector3(xPos, ChipPrefab.transform.position.y, zPos), Quaternion.identity);
+        temp.transform.parent = ChipParent.transform;
+    }
+
+    void UpdateChipAmount(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            ChipInstantiation();
+        }
+    }
+}
---
+++ MollysPlaygroung/Assets/Scripts/Gameplay/Greed/PlayerGreed.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGreed : MonoBehaviour
+{
+    bool carryChip;
+    bool withinZone;
+
+    public Rigidbody rb;
+    public float moveSpeed;
+
+    Vector3 keyboardMovement;
+
+    GameObject currentChip;
+    GameObject currentZone;
+
+    public List<GameObject> carriedChips = new List<GameObject>();
+
+    public int throwCount;
+    public int carryCount;
+
+    void Start()
+    {
+        carryChip = false;
+        withinZone = true;
+        throwCount = 0;
+    }
+
+    void Update()
+    {
+        keyboardMovement.x = Input.GetAxisRaw("Horizontal");
+        keyboardMovement.z = Input.GetAxisRaw("Vertical");
+
+        if (!GameObject.Find("GameManager").GetComponent<GreedGameManager>().diceProcedure)
+        {
+            if (throwCount >= GameObject.Find("GameManager").GetComponent<GreedGameManager>().diceValueRolled)
+            {
+                GameObject.Find("GameManager").GetComponent<GreedGameManager>().diceProcedure = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                if (carryChip && currentChip != null && carryCount < GameObject.Find("GameManager").GetComponent<GreedGameManager>().diceValueRolled)
+                {
+                    if (currentChip.GetComponent<ChipScript>().Available)
+                    {
+                        if (carriedChips.Count == 0)
+                        {
+                            GameObject.Find("GameManager").GetComponent<GreedGameManager>().FirstCarriedChip(currentChip, this.gameObject, carriedChips.Count);
+                        }
+                        else
+                        {
+                            GameObject.Find("GameManager").GetComponent<GreedGameManager>().CarryMoreChips(currentChip, this.gameObject, carriedChips[carriedChips.Count - 1], carriedChips.Count);
+                        }
+
+                        carriedChips.Add(currentChip);
+                        currentChip = null;
+
+                        carryCount++;
+                    }
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (carriedChips.Count > 0 && withinZone)
+                {
+                    GameObject.Find("GameManager").GetComponent<GreedGameManager>().DropTheChip(carriedChips[carriedChips.Count - 1], this.gameObject, currentZone);
+
+                    carriedChips.RemoveAt(carriedChips.Count - 1);
+                    throwCount++;
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                GameObject.Find("GameManager").GetComponent<GreedGameManager>().RollingTheDice();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + keyboardMovement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ChipZone")
+        {
+            withinZone = true;
+            currentZone = other.gameObject;
+        }
+
+        if (other.tag == "Chip" && currentChip == null)
+        {
+            carryChip = true;
+            currentChip = other.transform.parent.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "ChipZone")
+        {
+            withinZone = false;
+            currentZone = null;
+        }
+
+        if (other.tag == "Chip")
+        {
+            carryChip = false;
+            currentChip = null;
+        }
+    }
+}
