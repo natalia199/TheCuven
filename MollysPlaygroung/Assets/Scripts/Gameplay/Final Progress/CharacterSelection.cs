23e1fe8bd97a67766c2600751a6b6a605ba03b26
natalia199 <ocamponati@Hotmail.com>
2023-03-09 20:52:10 -0500

    non networked character selection

--- MollysPlaygroung/Assets/Scripts/Gameplay/Final Progress/CharacterSelection.cs
+++ MollysPlaygroung/Assets/Scripts/Gameplay/Final Progress/CharacterSelection.cs
@@ -9,10 +9,17 @@ public class CharacterSelection : MonoBehaviour
     public GameObject Target;
     public GameObject previousTarg;
     public GameObject spotLight;
+
+    
     public GameObject charInfo;
     public Material clear;
     public float babyLift;
     public float regPos;
+
+    public float bottomRowSit;
+    public float bottomRowStand;
+    public float topRowSit;
+    public float topRowStand;
 
     public List<GameObject> characters = new List<GameObject>();
 
@@ -33,6 +40,54 @@ public class CharacterSelection : MonoBehaviour
                 Debug.Log("hit " + hit.transform.gameObject.name);
                 if (hit.collider.gameObject.tag == "CharacterChoice")
                 {
+                    // OP 2
+                    /*
+                    if (previousTarg != null)
+                    {
+                        for (int i = 0; i < characters.Count; i++)
+                        {
+                            if (characters[i].name == previousTarg.name)
+                            {
+                                previousTarg.transform.GetChild(0).gameObject.SetActive(false);
+
+                                if (i < 4)
+                                {
+                                    previousTarg.transform.position = new Vector3(previousTarg.transform.position.x, topRowSit, previousTarg.transform.position.z);
+                                    break;
+                                }
+                                else if (i >= 4)
+                                {
+                                    previousTarg.transform.position = new Vector3(previousTarg.transform.position.x, bottomRowSit, previousTarg.transform.position.z);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    previousTarg = hit.collider.gameObject;
+
+                    for (int i = 0; i < characters.Count; i++)
+                    {
+                        hit.collider.transform.GetChild(0).gameObject.SetActive(true);
+
+                        if (characters[i].name == hit.collider.gameObject.name)
+                        {
+                            if (i < 4)
+                            {
+                                hit.collider.gameObject.transform.position = new Vector3(hit.collider.gameObject.transform.position.x, topRowStand, hit.collider.gameObject.transform.position.z);
+                                break;
+                            }
+                            else if (i >= 4)
+                            {
+                                hit.collider.gameObject.transform.position = new Vector3(hit.collider.gameObject.transform.position.x, bottomRowStand, hit.collider.gameObject.transform.position.z);
+                                break;
+                            }
+                        }
+                    }
+                    */
+
+                    // OP 3
+                    
                     if (previousTarg != null)
                     {
                         previousTarg.transform.position = new Vector3(previousTarg.transform.position.x, regPos, previousTarg.transform.position.z);
@@ -46,10 +101,40 @@ public class CharacterSelection : MonoBehaviour
                     Target.GetComponent<MeshRenderer>().material = hit.collider.gameObject.GetComponent<MeshRenderer>().material;
                     hit.collider.gameObject.transform.position = new Vector3(hit.collider.gameObject.transform.position.x, babyLift, hit.collider.gameObject.transform.position.z);
                     spotLight.SetActive(true);
+                    
                     break;
                 }
                 else if (hit.collider.gameObject.tag == "Ignore")
                 {
+                    // OP 2
+                    /*
+                    if (previousTarg != null)
+                    {
+                        previousTarg.transform.GetChild(0).gameObject.SetActive(false);
+
+                        for (int i = 0; i < characters.Count; i++)
+                        {
+                            if (characters[i].name == previousTarg.name)
+                            {
+                                if (i < 4)
+                                {
+                                    previousTarg.transform.position = new Vector3(previousTarg.transform.position.x, topRowSit, previousTarg.transform.position.z);
+                                    break;
+                                }
+                                else if (i >= 4)
+                                {
+                                    previousTarg.transform.position = new Vector3(previousTarg.transform.position.x, bottomRowSit, previousTarg.transform.position.z);
+                                    break;
+                                }
+                            }
+                        }
+                    }                    
+
+                    previousTarg = null;
+                    */
+
+                    // OP 3
+                    
                     if (previousTarg != null)
                     {
                         previousTarg.transform.position = new Vector3(previousTarg.transform.position.x, regPos, previousTarg.transform.position.z);
@@ -61,7 +146,8 @@ public class CharacterSelection : MonoBehaviour
 
                     Target.GetComponent<MeshRenderer>().material = clear;
                     spotLight.SetActive(false);
-                }
+                   
+                } 
             }
         }
 
