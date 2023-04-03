2e06f97779c7b7787cfdef9baff42bf34584f9f8
Zach van Noppen <dzachvannoppen@gmail.com>
2023-04-03 14:40:04 -0400

    Trimmed audio

--- MollysPlaygroung/Assets/Scripts/Gameplay/Final Progress/SoundManager.cs
+++ MollysPlaygroung/Assets/Scripts/Gameplay/Final Progress/SoundManager.cs
@@ -8,6 +8,21 @@ public class SoundManager : MonoBehaviour
     public AudioClip smacking;
     public AudioClip jumping;
     public AudioClip gettingPushed;
+    public AudioClip bearTrapClose;
+    public AudioClip bearTrapDrop;
+    public AudioClip bearTrapOpen;
+    public AudioClip chipDrop;
+    public AudioClip chipStack;
+    public AudioClip diceRoll;
+    public AudioClip dizzy;
+    public AudioClip falling;
+    public AudioClip lightFlicker;
+    public AudioClip meatSlap;
+    public AudioClip munching;
+    public AudioClip poisoned;
+    public AudioClip rustyCapybara;
+    public AudioClip waterDrink;
+    public AudioClip waterPour;
 
     public float volume = 1;
 
@@ -30,4 +45,80 @@ public class SoundManager : MonoBehaviour
     {
         AudioSource.PlayClipAtPoint(jumping, GameObject.Find(pname).transform.position, volume);
     }
+
+    public void bearTrapCloseSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(bearTrapClose, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void bearTrapDropSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(bearTrapDrop, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void bearTrapOpenSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(bearTrapOpen, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void chipDropSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(chipDrop, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void chipStackSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(chipStack, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void diceRollSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(diceRoll, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void dizzySFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(dizzy, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void fallingSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(falling, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void lightFlickerSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(lightFlicker, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void meatSlapSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(meatSlap, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void poisonedSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(poisoned, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void munchingSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(munching, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void rustyCapybaraSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(rustyCapybara, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void waterDrinkSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(waterDrink, GameObject.Find(pname).transform.position, volume);
+    }
+
+    public void waterPourSFX(string pname)
+    {
+        AudioSource.PlayClipAtPoint(waterPour, GameObject.Find(pname).transform.position, volume);
+    }
+
 }
